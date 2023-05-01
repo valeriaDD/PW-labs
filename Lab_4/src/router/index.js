67a6626410b83b951dbab76f2e2456aa1e47e02a
valeria dubina <valeriadubina0304@gmail.com>
2023-05-01 21:56:44 +0300

    Configure redirections to log in and save user info

--- Lab_4/src/router/index.js
+++ Lab_4/src/router/index.js
@@ -4,6 +4,7 @@ import DashboardPage from "@/components/DashboardPage.vue";
 import QuizzesPage from "@/components/QuizzesPage.vue";
 import QuizPage from "@/components/QuizPage.vue";
 import ErrorPage from "@/components/ErrorPage.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
@@ -36,6 +37,18 @@ const routes = [
 const router = new VueRouter({
     routes,
     mode: 'history'
+})
+
+router.beforeEach(async (to, from, next) => {
+    if (!store.getters['profile/getUser'] && to.name !== 'Dashboard') {
+        return next({ name: 'Dashboard' })
+    }
+
+    if (store.getters['profile/getUser'] && to.name === 'Dashboard') {
+        return next({ name: 'Quizzes' })
+    }
+
+    return next()
 })
 
 export default router;
--- Lab_4/src/store/modules/profile.js
+++ Lab_4/src/store/modules/profile.js
@@ -1,15 +1,19 @@
 import {createUser} from "@/services/restAPIs";
 
 const state = {
-    user: {},
+    userId: localStorage.getItem('id') ?? undefined,
+    completedQuizzes:  localStorage.getItem('completed') ?? [],
     request: {
-        profile: false,
+        started: false,
     },
 };
 
 const getters = {
     getUser(state) {
-        return state.user
+        return state.userId
+    },
+    getCompletedQuizzes(state) {
+        return state.completedQuizzes
     }
 };
 
@@ -18,20 +22,38 @@ const actions = {
         commit("SET_REQUEST_START");
         try {
             const response = await createUser({name: name, surname: surname})
-            console.log(response);
+            commit("SET_USER", response);
             commit("SET_REQUEST_END");
         } catch (e) {
             commit("SET_REQUEST_END");
         }
-    }
+    },
+    addFinishedQuiz({commit}, {id, score}) {
+        commit("ADD_COMPLETED_QUIZ", id, score)
+    },
+    logout({ commit }) {
+        commit('REMOVE_USER')
+    },
 };
 
 const mutations = {
+    SET_USER(state, user) {
+      state.userId = user.id;
+      localStorage.setItem('id', user.id);
+    },
+    REMOVE_USER(state) {
+        state.userId = undefined;
+        localStorage.removeItem('id');
+    },
+    ADD_COMPLETED_QUIZ(state, id, score) {
+        state.completedQuizzes.push({id: id, score:score})
+        localStorage.setItem('completed', state.completedQuizzes);
+    },
     SET_REQUEST_START(state) {
-        state.request.profile = true;
+        state.request.started = true;
     },
     SET_REQUEST_END(state) {
-        state.request.profile = false;
+        state.request.started = false;
     }
 };
 

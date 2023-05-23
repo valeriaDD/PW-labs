2d3fe9b1a513f20fe7a83d2d00e9766087fbe54e
valeria dubina <valeriadubina0304@gmail.com>
2023-05-23 23:05:52 +0300

    Add start and list commands

--- Lab_5/NewsAPI.py
+++ Lab_5/NewsAPI.py
@@ -12,12 +12,8 @@ class NewsAPI:
         # result = []
         #
         # for article in articles:
-        #     article_data = {
-        #         'url': article['url'],
-        #         'title': article['title'],
-        #         'author': article['author']
-        #     }
+        #     article_data = article['title'] + "\r\nAuthor: " + article['author'] + "\r\n" + article['url']
         #     result.append(article_data)
         #
         # return result
-        return [{"author":"Matias Grez","title":"Carmelo Anthony, 10-time NBA All-Star and one of basketball\u2019s greatest scorers, announces retirement - CNN","url":"https://www.cnn.com/2023/05/22/sport/carmelo-anthony-announces-retirement-spt-intl/index.html"},{"author":"HT News Desk","title":"Army deployed as fresh violence erupts in Manipur, houses set on fire in Imphal - Hindustan Times","url":"https://www.hindustantimes.com/india-news/fresh-manipur-violence-abandoned-houses-set-on-fire-in-imphal-curfew-army-101684755288846.html"},{"author":"South China Morning Post","title":"Protests over nukes as G7 leaders talk Ukraine and China in Hiroshima - South China Morning Post","url":"https://news.google.com/rss/articles/CCAiC3A0WTVVUnUzQ2NvmAEB?oc=5"},{"author":"By  WALLA! HEALTH","title":"These daily habits can impact your gut health - The Jerusalem Post","url":"https://www.jpost.com/health-and-wellness/article-743704"}]
+        return ["Carmelo Anthony, 10-time NBA All-Star and one of basketball\u2019s greatest scorers, announces retirement - CNN\r\nAuthor: Matias Grez\r\nhttps://www.cnn.com/2023/05/22/sport/carmelo-anthony-announces-retirement-spt-intl/index.html","Army deployed as fresh violence erupts in Manipur, houses set on fire in Imphal - Hindustan Times\r\nAuthor: HT News Desk\r\nhttps://www.hindustantimes.com/india-news/fresh-manipur-violence-abandoned-houses-set-on-fire-in-imphal-curfew-army-101684755288846.html","Protests over nukes as G7 leaders talk Ukraine and China in Hiroshima - South China Morning Post\r\nAuthor: South China Morning Post\r\nhttps://news.google.com/rss/articles/CCAiC3A0WTVVUnUzQ2NvmAEB?oc=5","These daily habits can impact your gut health - The Jerusalem Post\r\nAuthor: By  WALLA! HEALTH\r\nhttps://www.jpost.com/health-and-wellness/article-743704"]
--- Lab_5/app.py
+++ Lab_5/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from NewsAPI import NewsAPI
+from telegram.ext import Updater, CommandHandler
 import os
 
 app = Flask(__name__)
@@ -13,5 +14,32 @@ def hello_world():
     return newsAPI.get_news('test')
 
 
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Wanna read some news?")
+
+
+def list(update, context):
+    if len(context.args) > 0:
+        user_says = " ".join(context.args)
+        print(user_says)
+
+    news_token = os.environ.get('NEWS_API_TOKEN')
+    newsAPI = NewsAPI(news_token)
+
+    news = newsAPI.get_news('test')
+
+    for new in news:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=new)
+
+
 if __name__ == "__main__":
+    token = '5857957671:AAF4O8AjoU1AGNqZ2DUUTknT-1CwTdRNFVw'
+    updater = Updater(token=token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('list', list))
+
+    updater.start_polling()
+
     app.run()
